1bbcc05a06fd8daeae3fdc0e75ba7345ca82aceb
Evgeniy <evgeniyr7@gmail.com>
2019-12-20 20:19:46 +0200

    feat(public-area): design for header

--- src/app/modules/shared-public/shared-public.module.ts
+++ src/app/modules/shared-public/shared-public.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicHeaderComponent } from './components/public-header/public-header.component';
 import { PublicFooterComponent } from './components/public-footer/public-footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [PublicHeaderComponent, PublicFooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [PublicHeaderComponent, PublicFooterComponent]
 })
