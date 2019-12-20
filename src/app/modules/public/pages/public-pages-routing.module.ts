fa31bdc9208c2d11c18b99d8ec39f563c0c0d09e
Evgeniy <evgeniyr7@gmail.com>
2019-12-20 19:11:20 +0200

    feat(public-area): check the route for public area

--- src/app/modules/public/pages/public-pages-routing.module.ts
+++ src/app/modules/public/pages/public-pages-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublicHomePageComponent } from './public-home-page/public-home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicHomePageComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
